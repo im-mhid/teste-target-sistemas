f1e30b4e3b04cdf10ec89541c6e60c6a94bde917
Matheus Dias <hmatheus031@gmail.com>
2024-09-04 11:35:57 -0300

    alterando dados para json predefinido

--- 3.js
+++ 3.js
@@ -8,60 +8,45 @@ a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 */
 
-const billings = {
-  1: 1000,
-  2: 200,
-  3: 300,
-  4: 400,
-  5: 500,
-  6: 0,
-  7: 0,
-  8: 2900,
-  9: 3000,
-  10: 100,
-  11: 1100,
-  12: 1200,
-  13: 0,
-  14: 0,
-  15: 1500,
-  16: 1600,
-  17: 1900,
-  18: 1800,
-  19: 1700,
-  20: 0,
-  21: 0,
-  22: 2200,
-  23: 2300,
-  24: 2400,
-  25: 2500,
-  26: 2600,
-  27: 0,
-  28: 0,
-  29: 800,
-  30: 900,
-};
+const billings = require("./dados.json");
 
-const filteredBillings = Object.values(billings).filter(value => value > 0);
-const lowerBilling = Math.min(...filteredBillings);
-const higherBilling = Math.max(...filteredBillings);
+const filteredBillings = Object.values(billings).filter(
+  value => value.valor >= 1,
+);
+const lowerBilling = filteredBillings.reduce((acc, curr) => {
+  if (curr.valor < acc.valor || acc.valor === undefined) {
+    return curr;
+  }
+  return acc;
+}, {});
+
+const higherBilling = filteredBillings.reduce((acc, curr) => {
+  if (curr.valor > acc.valor || acc.valor === undefined) {
+    return curr;
+  }
+  return acc;
+}, {});
+
 const averageBilling = Object.values(billings).reduce((acc, curr) => {
-  if (curr > 0) {
-    return acc + curr;
+  if (curr.valor >= 1) {
+    return acc + curr.valor;
   }
   return acc;
 }, 0);
 const daysAboveAverageList = Object.entries(billings).reduce(
   (acc, [key, value]) => {
-    if (value > averageBilling / filteredBillings.length) {
-      acc[key] = value;
+    if (value.valor > averageBilling / filteredBillings.length) {
+      acc[value.dia] = value;
     }
     return acc;
   },
   {},
 );
 
-console.log(daysAboveAverageList);
-
-console.log(lowerBilling); // 0
-console.log(higherBilling); // 3000
+console.log(
+  `O menor faturamento foi de ${lowerBilling.valor} no dia ${lowerBilling.dia}`,
+);
+console.log(
+  `O maior faturamento foi de ${higherBilling.valor} no dia ${higherBilling.dia}`,
+);
 console.log(daysAboveAverageList);
